2c76c2053eeb8270075905351d0671057b6ebdcd
BuddiReddy Murali <muraliraju800@gmail.com>
2026-08-12 10:04:13 +0000

    incremental

---
+++ models/incremental/fact_orders_increme.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='incremental',
+    unique_key='order_id',
+    incremental_strategy='merge'
+) }}
+
+select
+    order_id,
+    customer_id,
+    order_date,
+    amount,
+    created_date date,
+    updated_date
+from {{ source('RAW', 'ORDERS') }}
+
+{% if is_incremental() %}
+where created_date >= (
+    select coalesce(
+        max(created_date),
+        '2026-08-12'::timestamp
+    )
+    from {{ this }}
+)
+{% endif %}
